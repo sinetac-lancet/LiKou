0a70b857b2732a299ee94a4aad90ac4b62481270
sinetac-lancet <18437952386@163.com>
2023-05-05 23:16:32 +0800

    20230505

--- OD机考原题/二叉树汇总.py
+++ OD机考原题/二叉树汇总.py
@@ -7,3 +7,34 @@
 
 
 # https://dream.blog.csdn.net/article/details/129083500
+
+
+
+import sys
+
+
+def solve_method(line):
+    split = line.split(" ")
+    ints = [0] * (len(split) + 1)
+    minValue = sys.maxsize
+    minPos = 0
+
+    for i in range(1, len(ints)):
+        ints[i] = int(split[i - 1])
+        if i > 1 and ints[i] != -1 and ints[i] < minValue:
+            minValue = ints[i]
+            minPos = i
+    way = []
+    while minPos >= 1:
+        way.append(ints[minPos])
+        minPos //= 2
+    way.reverse()
+
+    for i in range(len(way)):
+        print(way[i], end='')
+        if i != len(way) - 1:
+            print(" ", end='')
+
+
+if __name__ == '__main__':
+    solve_method(input())
